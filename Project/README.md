90263364f1577ff377d7f0760121feb46cdc6b1c
Andrew Bigoney (US) <abigoney@battelleecology.org>
2024-02-19 17:02:15 -0700

    Updated with feedback from first submition

--- Project/README.md
+++ Project/README.md
@@ -10,27 +10,20 @@ U.S. City Demographic Data: OpenSoft - https://public.opendatasoft.com/explore/d
 Airport Code Table: DataHub.io - https://datahub.io/core/airport-codes#data
 
 Data Model
-The purpose of the final data model for this project is to analyze the relationship between airports, temperature, and population to determine if airports have a greater influence on temperatures than population. The data model is designed to support efficient querying and analysis, enabling an examination of airport data conjunction with temperature and demographic data.
-By integrating these datasets, we can explore the correlation between airport presence and temperature variations, as well as the influence of population density on temperature changes.  
-Furthermore, this data can be valuable for veterans seeking a cool place to live near other veterans with numerous airports. The data model can assist in identifying areas with high concentrations of airports and favorable temperature ranges. The `demo` function in `etl.py` provides example queries that can help such users.
+The purpose of the final data model for this project is to analyze the relationship between airports, temperature, and population to determine if airports have a greater influence on temperatures than population. The data model is designed to support efficient querying and analysis, enabling a comprehensive examination of airport data conjunction with temperature and demographic data.
+The data model is designed to support efficient querying and analysis, enabling a comprehensive examination of temperature data in relation to airport locations and population density. By integrating these datasets, we can explore the correlation between airport presence and temperature variations, as well as the influence of population density on temperature changes.
 
 Project Steps
 Data Exploration: In this step, I explored the provided datasets to gain a better understanding of the data structure, quality, and potential challenges.
 
-Data Cleaning and Transformation: Based on the insights gained from the exploration step, I cleaned and transform the data to ensure its quality and compatibility with the goals of the final data model.
+Data Cleaning and Transformation: Based on the insights gained from the exploration step, I cleaned and transform the data to ensure its quality and compatibility with the final data model.
 
 Data Pipeline Design: To design a scalable and efficient data pipeline, I have utilized AWS EMR with Apache Spark. The pipeline follows a systematic process of ingesting, cleaning, transforming, and loading the data into the final data model. The pipeline also includes writing the results to parquet files for further analysis and storage.
 By leveraging the power of AWS EMR and Apache Spark, the data pipeline ensures high scalability, allowing it to handle large volumes of data effectively. The pipeline takes advantage of Spark's distributed computing capabilities, enabling parallel processing and optimizing performance.
 
-Data Quality Checks: In order to ensure the accuracy, completeness, and consistency of the data throughout the pipeline, I have implemented several data quality checks. These checks are crucial for maintaining the integrity of the data and ensuring its suitability for analysis.
-Firstly, I have included a data availability check to confirm that the data can be successfully read in from the S3 bucket. This check ensures that the necessary data is accessible and ready for processing.
-Secondly, I have implemented a data count check to compare the number of records read from the data source with the number of records written to the destination. This check helps to identify any discrepancies or data loss during the ETL process.
-Lastly, I have included a duplicate row check to ensure that there are no duplicate entries in the data. This check helps to maintain data consistency and avoid any potential issues that may arise from duplicate records.
-By implementing these data quality checks, I can confidently verify the accuracy, completeness, and consistency of the data at various stages of the pipeline. This ensures that the final data model is reliable and suitable for the intended purpose.
+Data Quality Checks: Throughout the pipeline, I have implemented data quality checks to ensure accurate, complete, and consistent data. Additionally, a QA check function has been included to verify data readability and suitability for running queries that align with the goals of the data model.
 
-Data Dictionary: I have created a comprehensive data dictionary named Data_Dictionary.md. This document contains detailed information about the final data model, including table schemas, column descriptions, and data types. It serves as a valuable resource for understanding the structure and content of the database.
-Additionally, I have included an ER diagram named ER_diagram.pdf. This diagram visually represents the relationships between the tables in an easy-to-understand format. By examining this diagram, you can quickly grasp the connections and dependencies among the tables.
-To query the data effectively, you can join the tables based on the city and municipality columns. This will allow you to retrieve all the relevant data related to these entities.
+Data Dictionary: I will have included `Data_Dictionary.md` as a data dictionary that provides detailed information about the final data model, including table schemas, column descriptions, and data types.
 
 Scaling for Increased Data: The pipeline is designed to easily scale to handle a 100x increase in data volume by simply increasing the number of nodes in the EMR cluster. This will allow the pipeline to handle larger amounts of data efficiently
 
--- Project/etl.py
+++ Project/etl.py
@@ -1,7 +1,7 @@
 import os
 import configparser
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col, avg, split
+from pyspark.sql.functions import udf, col, avg, split, countDistinct, monotonically_increasing_id, count
 from pyspark.sql.types import FloatType
 import pyspark.sql.functions as F
 
@@ -120,7 +120,7 @@ def read_airport_codes(spark, input_bucket, input_file):
     """
     Rename the name column for uniqueness
     """
-    airport_df = airport_df.withColumnRenamed("name", "airtport_name")
+    airport_df = airport_df.withColumnRenamed("name", "airport_name")
 
     """
     Limit the data to only US
@@ -263,40 +263,63 @@ def make_tables(temp_bucket, airport_df, city_ground_temp_df, demographics_df):
         "gtemp.average_temperature_uncertainty",
         "gtemp.city",
         "gtemp.country",
-        "gtemp.latitude",
-        "gtemp.longitude",
         "gtemp.year",
         "gtemp.mon",
         "air.ident",
         "air.type",
-        "air.airtport_name",
         "air.elevation_ft",
         "air.continent",
         "air.iso_country",
         "air.iso_region",
-        "air.municipality",
         "air.gps_code",
         "air.iata_code",
         "air.local_code",
         "demog.ave_household_size",
         "demog.count",
-        "demog.female_population",
-        "demog.foreign_born",
-        "demog.male_population",
-        "demog.median_age",
-        "demog.veteran_population",
-        "demog.race",
         "demog.state",
         "demog.state_code",
         "demog.total_population"
     )
 
+    # Add a primary key column
+    fact_table = fact_table.withColumn("id", monotonically_increasing_id())
+
     """
     Create dimension tables
     """
-    dimension_table_airport = airport_df.select("municipality", "elevation_ft", "airtport_name", "iso_country", "iso_region")
-    dimension_table_demographics = demographics_df.select("city", "state", "median_age", "male_population", "female_population", "total_population", "veteran_population", "foreign_born", "ave_household_size", "state_code", "race", "count")
+    dimension_table_airport = airport_df.select("municipality", 
+                                                "elevation_ft", 
+                                                "airport_name", 
+                                                "iso_country", 
+                                                "iso_region", 
+                                                "latitude", 
+                                                "longitude",)
+    
+    dimension_table_demographics = demographics_df.select("city", 
+                                                          "state", 
+                                                          "median_age", 
+                                                          "male_population", 
+                                                          "female_population", 
+                                                          "total_population", 
+                                                          "veteran_population", 
+                                                          "foreign_born", 
+                                                          "ave_household_size", 
+                                                          "state_code", 
+                                                          "race", 
+                                                          "count")
 
+    """
+    Lets drop any duplicates
+    """
+    fact_table = fact_table.dropDuplicates()
+    dimension_table_airport = dimension_table_airport.dropDuplicates()
+    dimension_table_demographics = dimension_table_demographics.dropDuplicates()
+    """
+    Count all the rows of the 3 tables for QA
+    """
+    org_fact_table_row_count = fact_table.count()
+    org_airport_table_row_count = dimension_table_airport.count()
+    org_demopraphics_table_row_count = dimension_table_demographics.count()
     """
     Write tables to Parquet files in the temp_bucket
     """
@@ -305,49 +328,112 @@ def make_tables(temp_bucket, airport_df, city_ground_temp_df, demographics_df):
     dimension_table_airport.write.mode("overwrite").parquet(os.path.join(output_folder, "dimension_table_airport"))
     dimension_table_demographics.write.mode("overwrite").parquet(os.path.join(output_folder, "dimension_table_demographics"))
 
+    return(org_fact_table_row_count, org_airport_table_row_count, org_demopraphics_table_row_count)
+
 """
     Run QA on the ingested data
 """
-def run_qa(spark):
+def run_qa(spark, org_fact_table_row_count=0, org_airport_table_row_count=0, org_demopraphics_table_row_count=0):
     """
     First QA we need to confirm the data can be read in
     """
     input_folder = os.path.join(temp_bucket, "capstone_output")
     qa_fact_table = spark.read.parquet(os.path.join(input_folder, "fact_table"))
     qa_dimension_table_airport = spark.read.parquet(os.path.join(input_folder, "dimension_table_airport"))
+    qa_dimension_table_demographics = spark.read.parquet(os.path.join(input_folder, "dimension_table_demographics"))
+    
+    """
+    QA by confirming the row counts of the data we read in match what we expect to write out
+    Start by conting the rows of the data we read in
+    """
+    qa_fact_table_row_count = qa_fact_table.count()
+    qa_airport_table_row_count = qa_dimension_table_airport.count()
+    qa_demopraphics_table_row_count = qa_dimension_table_demographics.count()
+    """
+    Compare the counts and let us know if they fail QA checks
+    """
+    if qa_fact_table_row_count == org_fact_table_row_count:
+        print("fact_table Passed first QA Check!!!!!!!!!!!!!!!!!!")
+    else:
+        print("************************************fact_table FAILD first QA Check!!!!!!!!!!!!!!!!!!")
+    if qa_airport_table_row_count == org_airport_table_row_count:
+        print("airport_table Passed first QA Check!!!!!!!!!!!!!!!!!!")
+    else:
+        print("************************************airport_table FAILD first QA Check!!!!!!!!!!!!!!!!!!")
+    if qa_demopraphics_table_row_count == org_demopraphics_table_row_count:
+        print("demopraphics_table Passed first QA Check!!!!!!!!!!!!!!!!!!")
+    else:
+        print("************************************demopraphics_table FAILD first QA Check!!!!!!!!!!!!!!!!!!")
+    
+    """
+    QA the tables to confirm that are no duplicate rows
+    Drop duplicates and recount the rows then check for changes in the counts
+    """
+    fact_table_without_duplicates = qa_fact_table.dropDuplicates()
+    row_count_after = fact_table_without_duplicates.count()
+    if row_count_after == org_fact_table_row_count:
+        print("fact_table Passed Second QA Check!!!!!!!!!!!!!!!!!!")
+    else:
+        print("************************************fact_table FAILD Second QA Check!!!!!!!!!!!!!!!!!!")
+        print(f"Had {org_fact_table_row_count} before dedup and {row_count_after} after")
+    airport_table_without_duplicates = qa_dimension_table_airport.dropDuplicates()
+    row_count_after = airport_table_without_duplicates.count()
+    if row_count_after == org_airport_table_row_count:
+        print("airport_table Passed Second QA Check!!!!!!!!!!!!!!!!!!")
+    else:
+        print("************************************airport_table FAILD Second QA Check!!!!!!!!!!!!!!!!!!")
+        print(f"Had {org_airport_table_row_count} before dedup and {row_count_after} after")
+    demopraphics_table_without_duplicates = qa_dimension_table_demographics.dropDuplicates()
+    row_count_after = demopraphics_table_without_duplicates.count()
+    if row_count_after == org_demopraphics_table_row_count:
+        print("demopraphics_table Passed Second QA Check!!!!!!!!!!!!!!!!!!")
+    else:
+        print("************************************demopraphics_table FAILD Second QA Check!!!!!!!!!!!!!!!!!!")
+        print(f"Had {org_demopraphics_table_row_count} before dedup and {row_count_after} after")
+
+
+def demo(spark):
+    """
+    This is an example of how users could use that data.
+    """
+    input_folder = os.path.join(temp_bucket, "capstone_output")
+    fact_table = spark.read.parquet(os.path.join(input_folder, "fact_table"))
+    dimension_table_airport = spark.read.parquet(os.path.join(input_folder, "dimension_table_airport"))
     dimension_table_demographics = spark.read.parquet(os.path.join(input_folder, "dimension_table_demographics"))
 
-    """
-    If we are successful at reading in the data, let's run a query on it to make sure it can be used for what we want it for
-    """
-    """
-    Calculate the average temperature by city
-    """
-    average_temperature_by_city = qa_fact_table.groupBy("city").agg(avg("average_temperature").alias("avg_temperature"))
 
     """
-    Join the average temperature with the airport elevation and city population
+    Example 1: 
+    This demonstrates how the data can be utilized to calculate the average temperature per city, taking into account the total population and the total number of airports. 
+    Additionally, users have the option to incorporate elevation data and latitude data to adjust the temperatures accordingly.
     """
-    result = average_temperature_by_city.join(qa_dimension_table_airport, average_temperature_by_city.city == qa_dimension_table_airport.municipality, "inner") \
-        .join(dimension_table_demographics, average_temperature_by_city.city == dimension_table_demographics.city, "inner")
+    joined_df = fact_table.join(dimension_table_demographics, fact_table.city == dimension_table_demographics.city, "inner") \
+                    .join(dimension_table_airport, fact_table.city == dimension_table_airport.municipality, "inner")
+
+    result_df = joined_df.groupBy(fact_table.city) \
+                    .agg(avg(fact_table.average_temperature).alias("average_temperature"),
+                         F.first(dimension_table_demographics.total_population).alias("total_population"),
+                         countDistinct(dimension_table_airport.airport_name).alias("total_airports"))
+    print(f" Example on how to to calculate the average temperature per city with the total population and total number of airports")
+    result_df.show()
+    
 
     """
-    Calculate the average temperature difference based on airport elevation and population
+    Example 2: 
+    Here is an example of how the data can be used to help a U.S. Veteran who is interested in warm climates and flying to find a new city to move to, where they can also connect with fellow veterans. 
+    Furthermore, the query can be adjusted to cater to someone who is interested in areas with higher female populations.
     """
-    result = result.withColumn("temperature_difference", result.avg_temperature - result.elevation_ft / 1000 - result.total_population / 1000000)
+    veterans_around_airports_df = joined_df.groupBy(fact_table.city) \
+                                    .agg(avg(fact_table.average_temperature).alias("average_temperature"),
+                                    F.first(dimension_table_demographics.veteran_population).alias("veteran_population"),
+                                    countDistinct(dimension_table_airport.airport_name).alias("total_airports"))
 
     """
-    Show the second QA result
+    Show the result DataFrame
     """
-    print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<QA Results>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-    result.show()
+    print(f"Example on how to using the data to find a home for a Veteran")
+    veterans_around_airports_df.show()
 
-    print(f"*************************************************************qa_fact_table**********************************************************")
-    qa_fact_table.show()
-    print(f"*************************************************************qa_dimension_table_airport**********************************************************")
-    qa_dimension_table_airport.show()
-    print(f"*************************************************************dimension_table_demographics**********************************************************")
-    dimension_table_demographics.show()
 
 def main():
     """
@@ -359,18 +445,25 @@ def main():
     Read in the input data
     """
     airport_df = read_airport_codes(spark, input_bucket, "airport-codes_csv.csv")
-    city_ground_temp_df = read_city_temp_data(spark, input_bucket, "Surface_Temps/GlobalLandTemperaturesByMajorCity.csv")
     demographics_df = read_cities_demographics(spark, input_bucket, "us-cities-demographics.json")
-
+    """
+    Select MajorCity.csv for a smaller data set to test with or use City.csv for the complete data set
+    """
+    #city_ground_temp_df = read_city_temp_data(spark, input_bucket, "Surface_Temps/GlobalLandTemperaturesByCity.csv")
+    city_ground_temp_df = read_city_temp_data(spark, input_bucket, "Surface_Temps/GlobalLandTemperaturesByMajorCity.csv")
     """
     Create the tables and write out the parquet files
     """
-    make_tables(temp_bucket, airport_df, city_ground_temp_df, demographics_df)
+    org_fact_table_row_count, org_airport_table_row_count, org_demopraphics_table_row_count = make_tables(temp_bucket, airport_df, city_ground_temp_df, demographics_df)
 
     """
     Run QA to make sure it all worked
     """
-    run_qa(spark)
+    run_qa(spark, org_fact_table_row_count, org_airport_table_row_count, org_demopraphics_table_row_count)
+    """
+    Run some example quries to demonstrate how the data can be used.
+    """
+    demo(spark)
 
 
 if __name__ == "__main__":
